49074121b14c454cc9afbff3dd3590bab3e774ca
Alex Papadatos <apapadatosdev@gmail.com>
2022-06-23 17:45:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 3000, () => {
